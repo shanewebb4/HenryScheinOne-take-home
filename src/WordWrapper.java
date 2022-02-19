b699f7bc89f2fe0858876c9c91a65b77cbf75d2d
shanewebb4 <shanewebb4@gmail.com>
2022-02-19 15:22:42 -0700

    function skeleton with first failing test

---
+++ src/WordWrapper.java
@@ -0,0 +1,13 @@
+public class WordWrapper {
+    // should break text into lines no longer than maxLineLength
+    // should break text on word boundaries by replacing a space with a newline char
+    // should output the resulting text
+    public static String wrap(String text, int maxLineLength) {
+
+        return "wrapper";
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/WordWrapperTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WordWrapperTest {
+    private final String toBeWrapped = "this text is wrapped";
+    private final String expected = "this\ntext\nis\nwrapped";
+
+    @Test
+    public void wrap() {
+        String actual = WordWrapper.wrap("wrap", 1);
+        assertEquals(actual, expected);
+    }
+}
