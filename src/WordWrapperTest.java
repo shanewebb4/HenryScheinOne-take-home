3e0518da81505e4b4227e0df1052a05dc355710b
shanewebb4 <shanewebb4@gmail.com>
2022-02-20 09:16:04 -0700

    second iteration of test

--- src/WordWrapperTest.java
+++ src/WordWrapperTest.java
@@ -4,11 +4,11 @@ import static org.junit.Assert.*;
 
 public class WordWrapperTest {
     private final String toBeWrapped = "this text is wrapped";
-    private final String expected = "this\ntext\nis\nwrapped\n";
+    private final String expected = "this text\nis\nwrapped\n";
 
     @Test
-    public void wrap() {
-        String actual = WordWrapper.wrap("wrap", 1);
+    public void wrapEightChars() {
+        String actual = WordWrapper.wrap(toBeWrapped, 8);
         assertEquals(actual, expected);
     }
 }
